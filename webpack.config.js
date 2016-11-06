acbb0b0cf8d62f884f6300690e167a77277d57e4
Alex Burner <alex.burner@gmail.com>
2016-11-06 15:53:51 -0800

    using CommonsChunkPlugin to create shared/bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,23 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'eval-cheap-module-source-map',
+  resolve: {
+    root: [
+      path.resolve('./docs')
+    ]
+  },
   entry: {
     entry1: './docs/entry1/index.js',
     entry2: './docs/entry2/index.js',
     entry3: './docs/entry3/index.js',
   },
   output: {
-    path: path.join(__dirname, 'docs'),
-    filename: '[name]/bundle.js'
+    filename: 'docs/[name]/bundle.js'
   },
-  resolve: {
-    root: [
-      path.resolve('./docs')
-    ]
-  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('docs/shared/bundle.js')
+  ],
   module: {
     loaders: [
       {
