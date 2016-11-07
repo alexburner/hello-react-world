01ccb00f7cbced8e36c422a7b30ed320bd5b00d1
Alex Burner <alex.burner@gmail.com>
2016-11-07 00:06:09 -0800

    adding webpack config comments

--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,29 @@ module.exports = {
     ]
   },
   entry: {
+    // these are where webpack looks
+    // for dependency trees to build
     entry1: './docs/entry1/index.js',
     entry2: './docs/entry2/index.js',
     entry3: './docs/entry3/index.js',
   },
   output: {
+    // this is where individual built chunks live
     filename: 'docs/[name]/bundle.js'
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('docs/shared/bundle.js'),
+    new webpack.optimize.CommonsChunkPlugin(
+      // this is where the common built chunk lives
+      'docs/shared/bundle.js'
+    ),
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
+      // this tells react to use its production mode
+      'process.env': {'NODE_ENV': JSON.stringify('production')}
     }),
     new webpack.optimize.DedupePlugin(),
     new webpack.SourceMapDevToolPlugin(),
     new webpack.optimize.UglifyJsPlugin({
+      // this tells uglify to stfu about react
       compress: { warnings: false },
     }),
   ],
